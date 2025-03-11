acf17b74ef9412611d9301b695517531eaff7664
Jiangqiu Shen <ivy22233qiu@live.com>
2025-03-10 21:37:39 -0400

    init update

---
+++ docs/intro.md
@@ -0,0 +1,2 @@
+1. 实现修改 transformers 来测量 perplexity 来测试效果
+2. 测试 sparseQ 效果
---
+++ src/main.py
@@ -0,0 +1,11 @@
+# %%
+import transformers.models
+
+
+if __name__ == "__main__":
+    import torch
+    import transformers
+    from transformers import BertTokenizer, BertModel
+    print(transformers.__version__)
+    pass
+# %%
