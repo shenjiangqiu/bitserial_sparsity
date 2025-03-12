8d0f640ce318d6bfe3a8bfd24fe4bc7078a473cc
Jiangqiu Shen <ivy22233qiu@live.com>
2025-03-12 18:23:59 +0000

    update sparQ

--- src/calculate_perplexity_main.py
+++ src/calculate_perplexity_main.py
@@ -1,15 +1,21 @@
 # %%
+import torch.cuda
+
+
 def main():
     import torch
     from transformers import AutoModelForCausalLM, AutoTokenizer
+    from transformers.models.llama import LlamaForCausalLM
 
     # 选择合适的设备
-    device = "mps"
-
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(device)
     # 加载模型和 tokenizer
-    model_name = "meta-llama/Meta-Llama-3-8B"
+    model_name = "meta-llama/Llama-3.2-3B-Instruct"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    model: LlamaForCausalLM = AutoModelForCausalLM.from_pretrained(model_name).to(
+        device
+    )
 
     def calculate_perplexity(model, tokenizer, text):
         # Tokenize 输入文本
@@ -17,10 +23,11 @@ def main():
         input_ids = encodings.input_ids.to(device)
 
         with torch.no_grad():
-            outputs = model(input_ids, labels=input_ids)
+            outputs = model(input_ids, labels=input_ids, sjq_sparse=2)
 
         # 计算 loss（交叉熵）
         loss = outputs.loss
+        print(loss)
         ppl = torch.exp(loss)
 
         return ppl.item()
